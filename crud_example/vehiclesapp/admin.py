624df848b671f291c88259abfdfe2afca896b25e
migueltovar <loganitsolutions@outlook.com>
2024-05-03 11:55:53 -0500

    ajustando el requirements.

--- crud_example/vehiclesapp/admin.py
+++ crud_example/vehiclesapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import vehicle
 # Register your models here.
+
+@admin.register(vehicle)
+class VehiculosAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placa', 'modelo','marca']
